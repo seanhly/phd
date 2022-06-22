2c69f3d1b67f000238aca02cce656abe302c1d30
Seán Healy <sean.healy@adaptcentre.ie>
2022-06-22 15:36:39 +0100

    Arch needs tmux

--- install234234.sh
+++ install234234.sh
@@ -1,7 +1,7 @@
 if [ -e /usr/bin/apt-get ]; then
 	apt-get -y install default-jre
 elif [ -e /usr/bin/pacman ]; then
-	yes y | pacman -S jre-openjdk-headless
+	yes y | pacman -S jre-openjdk-headless tmux
 fi	
 pip3 install grobid-tei-xml dateparser
 /usr/bin/git clone https://github.com/seanhly/phd
