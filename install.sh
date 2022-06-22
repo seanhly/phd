ee919880d297ed385304fcf3693117f119765fab
Seán Healy <sean.healy@adaptcentre.ie>
2022-06-22 11:42:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 apt install python3
 if [ -e /usr/bin/phd ]; then
-	/usr/bin/phd run-grobid
+	#/usr/bin/phd run-grobid
+	echo ok
 else
 	/usr/bin/git clone https://github.com/seanhly/phd
 	(cd phd && make install)
