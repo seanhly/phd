60e73a56642fb99dfe247bbde50f1f6e287038a9
Seán Healy <sean.healy@adaptcentre.ie>
2022-07-05 00:56:13 +0100

    Logs

--- src/arguments/OptionArgument.py
+++ src/arguments/OptionArgument.py
@@ -13,33 +13,49 @@ class OptionArgument(Argument):
 	def __str__(self):
 		return self.option
 
-	def parse_argument_for_action(self, arguments, current_index, action):
+	def parse_argument_for_action(
+		self,
+		arguments,
+		current_index,
+		action: "UserAction"
+	):
+		from user_actions.UserAction import UserAction
+		the_action: UserAction = action
 		current_index += 1
-		if self.option in action.disqualified_options:
-			action.conflicting_options[self.option] = action.indexed_blocking_options()[self.option]
-		if self.option not in action.recognised_options():
-			action.unrecognised_options.add(self.option)
+		if self.option in the_action.disqualified_options:
+			the_action.conflicting_options[self.option] = the_action.indexed_blocking_options()[self.option]
+		if self.option not in the_action.recognised_options():
+			print(f"Unrecognised: {self.option}")
+			the_action.unrecognised_options.add(self.option)
 		else:
-			action.options[self.option] = None
-			if self.option in action.indexed_obligatory_option_groups():
-				for o in action.indexed_obligatory_option_groups()[self.option]:
-					del action.indexed_obligatory_option_groups()[o]
-			if self.option in action.indexed_blocking_options():
-				for o in action.indexed_blocking_options()[self.option]:
-					action.disqualified_options[o] = self.option
-			if self.option in action.arg_options():
+			print(f"Recognised: {self.option}")
+			the_action.options[self.option] = None
+			if self.option in the_action.indexed_obligatory_option_groups():
+				print(f"\tObligatory: {self.option}")
+				for o in the_action.indexed_obligatory_option_groups()[self.option]:
+					del the_action.indexed_obligatory_option_groups()[o]
+			if self.option in the_action.indexed_blocking_options():
+				print(f"\tBlocking: {self.option}")
+				for o in the_action.indexed_blocking_options()[self.option]:
+					the_action.disqualified_options[o] = self.option
+			if self.option in the_action.arg_options():
+				print(f"\tArg option: {self.option}")
 				if current_index >= len(arguments):
-					action.missing_argument_for_options.append(self.option)
+					print(f"\tEOL: {self.option}")
+					the_action.missing_argument_for_options.append(self.option)
 				else:
 					argument = arguments[current_index]
 					if type(argument) == type(self):
-						action.missing_argument_for_options.append(self.option)
-					elif type(argument) != action.options[self.option]:
+						print(f"\tOther arg: {self.option}")
+						the_action.missing_argument_for_options.append(self.option)
+					elif type(argument) != the_action.options[self.option]:
+						print(f"\tOther type: {self.option}")
 						current_index += 1
-						action.incorrect_argument_type_for_options.append((self.option, argument))
+						the_action.incorrect_argument_type_for_options.append((self.option, argument))
 					else:
+						print(f"\tAll good: {self.option} {argument}")
 						current_index += 1
-						action.options[self.option] = argument
+						the_action.options[self.option] = argument
 
 		return current_index
 			
