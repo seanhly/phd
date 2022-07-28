67e7a9ce207b95e42b34603e62e991c3bb1892f6
Seán Healy <sean.healy@adaptcentre.ie>
2022-07-28 19:13:21 +0100

    Fix typo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "Clone repository..."
 /usr/bin/git clone https://github.com/seanhly/phd /phd
 echo "Make..."
-(cd phd && make install)
+(cd /phd && make install)
 echo "Install and start..."
 phd install
